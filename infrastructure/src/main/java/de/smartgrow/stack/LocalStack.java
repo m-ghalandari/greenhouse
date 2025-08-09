2ee4fdf6be759f4b9db77e486d14dc79fc7bd4c9
Mamali99 <mohammadghalandari1998@gmail.com>
2025-08-09 11:13:59 +0200

    created a AWS resource (Vpc) in the LocalStack class

--- infrastructure/src/main/java/de/smartgrow/stack/LocalStack.java
+++ infrastructure/src/main/java/de/smartgrow/stack/LocalStack.java
@@ -1,11 +1,23 @@
 package de.smartgrow.stack;
 
+import software.amazon.awscdk.services.ec2.Vpc;
 import software.amazon.awscdk.*;
 
 public class LocalStack extends Stack {
+    private final Vpc vpc;
 
     public LocalStack(final App scope, final String id, final StackProps props){
         super(scope, id, props);
+        this.vpc = createVpc();
+    }
+
+    private Vpc createVpc() {
+        return Vpc.Builder
+                .create(this, "GreenhouseManagementVPC")
+                .vpcName("GreenhouseManagementVPC")
+                .maxAzs(2)
+                .build();
+
     }
 
     public static void main(final String[] args){
